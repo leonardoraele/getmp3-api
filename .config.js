88c4a4180821c22e19d60fabe1e597e57e45fde9
Leonardo Raele <leonardoraele@gmail.com>
2021-08-18 22:20:02 -0300

    Adding env var PORT as alternative for server port

--- .config.js
+++ .config.js
@@ -2,6 +2,6 @@ require('dotenv/config');
 
 module.exports =
 {
-	SERVER_PORT: process.env.SERVER_PORT ?? 3100,
+	SERVER_PORT: process.env.PORT ?? process.env.SERVER_PORT ?? 3100,
 	SERVER_HOST: process.env.SERVER_HOST ?? '127.0.0.1',
 };

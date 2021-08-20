7110a563babcdba33f331100504523e7a72b13c6
Leonardo Raele <leonardoraele@gmail.com>
2021-08-20 01:16:47 -0300

    Changin default server hostname from 127.0.0.1 to 0.0.0.0

--- .config.js
+++ .config.js
@@ -3,5 +3,5 @@ require('dotenv/config');
 module.exports =
 {
 	SERVER_PORT: process.env.PORT ?? process.env.SERVER_PORT ?? 3100,
-	SERVER_HOST: process.env.SERVER_HOST ?? '127.0.0.1',
+	SERVER_HOST: process.env.SERVER_HOST ?? '0.0.0.0',
 };

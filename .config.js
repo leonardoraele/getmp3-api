09f72872d045ce2af28abb093fe82426e2159619
Leonardo Raele <leonardoraele@gmail.com>
2022-01-07 09:18:28 -0300

    [ENH] Added generic '/download' route

--- .config.js
+++ .config.js
@@ -5,4 +5,5 @@ module.exports =
 	SERVER_PORT: process.env.PORT ?? process.env.SERVER_PORT ?? 3100,
 	SERVER_HOST: process.env.SERVER_HOST ?? '0.0.0.0',
 	FRONTEND_PATH: process.env.FRONTEND_PATH,
+	SERVER_API_PREFIX: process.env.SERVER_API_PREFIX ?? '/',
 };
